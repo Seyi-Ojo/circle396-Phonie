12e97926c22d38271680234302a65b860929be84
Mayowa Ojo <81079566+Seyi-Ojo@users.noreply.github.com>
2022-08-01 22:19:40 +0000

    moved the project to github

--- app.mjs
+++ app.mjs
@@ -1,16 +1,38 @@
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
+  let mtnPrefixes = ["07025", "07026", "0703", "0704", "0706", "0803", "0806", "0810", "0813", "0814", "0816", "0903", "0906", "0916"];
+
+  let gloPrefixes = ["0805", "0705", "0905", "0807", "0815", "0811", "0905"];
   
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
+  let nineMobilePrefixes = ["0809", "0909", "0817", "0818"];
   
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
+  let airtelPrefixes = ["0802", "0902", "0701", "0808", "0708", "0812"];
+  
+  const element = document.getElementById("phone");
+  const network = document.getElementById("network");
+  element.addEventListener("input", function(event) {
+    const phoneNumber = event.target.value;
+    
+    let isMTN = mtnPrefixes.some(prefix => phoneNumber.startsWith(prefix));
+    if (isMTN) {
+      network.setAttribute("src", "mtn.webp");
     }
-  };
+    let isGlo = gloPrefixes.some(prefix => phoneNumber.startsWith(prefix));
+    if (isGlo) {
+      network.setAttribute("src", "glo.jpeg");
+    }
+  
+    let isnineMobile = nineMobilePrefixes.some(prefix => phoneNumber.startsWith(prefix));
+    if (isnineMobile) {
+      network.setAttribute("src", "nineMobile.png");
+    }
+  
+    let isairtel = airtelPrefixes.some(prefix => phoneNumber.startsWith(prefix));
+    if (isairtel) {
+      network.setAttribute("src", "airtel.png");
+    }
+  });
+  
+};
   
   // ======= DO NOT EDIT ============== //
   export default startApp;
